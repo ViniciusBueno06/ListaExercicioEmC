d9aef26a5093fc841d61d2df37165651a88c4055
Vinicius Bueno <vini.buenog@gmail.com>
2025-08-21 18:48:32 -0300

    Add files via upload

---
+++ ExFixacao-atp2.cpp
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <string.h>
+#include <conio2.h>
+
+int main(void){
+	int aux,opcao,TF;
+	do{
+		
+		printf("========= Exercicios de fixacao ATPII - Struct =========");
+		printf("Digite o numero do exercicio desejado:");
+		scanf("%d",&opcao);
+		switch(opcao){
+			case 1:printf("Exercicio 1");
+				char AuxNome[25];
+				TF=5;
+				struct tp_pessoa{
+					char nome[25],endereco[40];
+					int telefone;
+				}tabPessoa[TF];
+				for(int i=0;i<TF;i++)
+				
+				
+				
+			
+			
+			
+			
+			
+			break;
+			
+			
+			
+			
+			
+			case 2:printf("");
+			break;
+			case 3:printf("");
+			break;
+			case 4:printf("");
+			break;
+			case 5:printf("");
+			break;
+			case 6:printf("");
+			break;
+			case 7:printf("");
+			break;
+			case 8:printf("");
+			break;
+			case 9:printf("");
+			break;
+			case 10:printf("");
+			break;
+			case 11:printf("");
+			break;
+			case 12:printf("");
+			break;
+			case 13:printf("");
+			break;
+			case 14:printf("");
+			break;
+			case 15:printf("");
+			break;
+			case 16:printf("");
+			break;
+			case 17:printf("");
+			break;
+			case 18:printf("");
+			break;
+			case 19:printf("");
+			break;
+			case 20:printf("");
+			break;
+			case 21:printf("");
+			break;
+			case 22:printf("");
+			break;
+			case 23:printf("");
+			break;
+			
+		}
+		
+	}
+	while(aux==27);{
+		
+		
+	}
+
+}
