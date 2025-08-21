5f522e0800516d262a285ab6d00d6cd01ea9bc5c
Vinicius Bueno <vini.buenog@gmail.com>
2025-08-21 20:46:41 -0300

    Update ExFixacao-atp2.cpp

--- ExFixacao-atp2.cpp
+++ ExFixacao-atp2.cpp
@@ -1,88 +1,74 @@
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
 #include <conio2.h>
+#include <string.h>
+#define TF_1 5
+
+struct TpData
+{
+	int dia,mes,ano;	
+};
+
+struct TpPessoa_1
+{
+	char nome[25],endereco[40];
+	int fone;
+};
 
 int main(void){
-	int aux,opcao,TF;
-	do{
-		
-		printf("========= Exercicios de fixacao ATPII - Struct =========");
-		printf("Digite o numero do exercicio desejado:");
-		scanf("%d",&opcao);
-		switch(opcao){
-			case 1:printf("Exercicio 1");
-				char AuxNome[25];
-				TF=5;
-				struct tp_pessoa{
-					char nome[25],endereco[40];
-					int telefone;
-				}tabPessoa[TF];
-				for(int i=0;i<TF;i++)
+	int cod;
+	printf("Digite o numero do exercicio:");
+	scanf("%d",&cod);
+	switch(cod){
+		case 1:
+			TpPessoa_1 TabPessoa[TF_1];
+			char AuxNome[25];
+			
+			int TLpessoa=0;
+			
+			printf("\n ======================= Ex 1 =======================\n");
+			printf("Nome[%d]: ",TLpessoa);
+			fflush(stdin);
+			gets(AuxNome);
+			
+			
+			while(TLpessoa<TF_1 && AuxNome[0]!='\0'){
 				
+				strcpy(TabPessoa[TLpessoa].nome,AuxNome);
+				printf("\nDigite o endereco da pessoa:");
+				fflush(stdin);
+				gets(TabPessoa[TLpessoa].endereco);
+				fflush(stdin);
+				printf("\nDigite o telefone da pessoa:");
+				scanf("%d",&TabPessoa[TLpessoa].fone);
+				TLpessoa++;
 				
-				
+				printf("Nome[%d]: ",TLpessoa);
+				fflush(stdin);
+				gets(AuxNome);
+			}
+			for(int i=0;i<TLpessoa-1;i++){
+				for(int j=i+1;j<TLpessoa;j++)
+					if(strcmp(TabPessoa[i].nome,TabPessoa[j].nome)==1){
+						strcpy(AuxNome,TabPessoa[j].nome);
+						strcpy(TabPessoa[j].nome,TabPessoa[i].nome);
+						strcpy(TabPessoa[i].nome,AuxNome);
+					}
+					
+			}
 			
+			for(int i=0;i<TLpessoa;i++){
+				printf("---------------------------------------------");
+				printf("\nNome: %s",TabPessoa[i].nome);
+				printf("\nEndereco: %s",TabPessoa[i].endereco);
+				printf("\nTelefone: %d",TabPessoa[i].fone);
+				printf("\n-------------------------------------------");
+			}
 			
-			
-			
-			
 			break;
-			
-			
-			
-			
-			
-			case 2:printf("");
-			break;
-			case 3:printf("");
-			break;
-			case 4:printf("");
-			break;
-			case 5:printf("");
-			break;
-			case 6:printf("");
-			break;
-			case 7:printf("");
-			break;
-			case 8:printf("");
-			break;
-			case 9:printf("");
-			break;
-			case 10:printf("");
-			break;
-			case 11:printf("");
-			break;
-			case 12:printf("");
-			break;
-			case 13:printf("");
-			break;
-			case 14:printf("");
-			break;
-			case 15:printf("");
-			break;
-			case 16:printf("");
-			break;
-			case 17:printf("");
-			break;
-			case 18:printf("");
-			break;
-			case 19:printf("");
-			break;
-			case 20:printf("");
-			break;
-			case 21:printf("");
-			break;
-			case 22:printf("");
-			break;
-			case 23:printf("");
-			break;
-			
-		}
-		
 	}
-	while(aux==27);{
-		
-		
-	}
-
+	
+	
+	
+	return 0;
 }
